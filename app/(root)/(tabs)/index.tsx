4e94d2c7fb3fcacc479ac79305ee495b36b94801
Hamza Haque <hamzahaque30@gmail.com>
2025-05-21 01:17:50 -0500

    Add configuration files for NativeWind and Tailwind CSS integration
    - Created babel.config.js to set up Babel presets for NativeWind and Expo.
    - Added metro.config.js to configure Metro bundler with NativeWind support.
    - Introduced tailwind.config.js for Tailwind CSS configuration, specifying content paths and presets.
    - Added global CSS file for Tailwind directives.
    - Updated package.json and package-lock.json to include NativeWind and Tailwind CSS dependencies.
    - Included nativewind-env.d.ts for TypeScript support with NativeWind.
    - Modified tsconfig.json to reference the new TypeScript definition file.
    - Updated _layout.tsx to import global CSS styles.
    - Enhanced index.tsx in the tabs directory to utilize Tailwind CSS classes for styling.

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "expo-router";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -10,10 +10,20 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="font-bold text-blue-500 text-lg my-10">
+        Welcome to Real Estate App
+      </Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Property</Link>
+      <Link
+        href={{
+          pathname: "/properties/[id]",
+          params: { id: "1" },
+        }} // This is the same as just writing "/properties/1" as the href directly as a string
+      >
+        Property
+      </Link>
     </View>
   );
 }
